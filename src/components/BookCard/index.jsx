3a737e08b1d6de253dbc0e2d056ed5814e49d6f9
Dhanesh Vishwakarma <dhanesh.vishwakarma11@gmail.com>
2021-09-05 22:20:09 +0530

    created Book card component

---
+++ src/components/BookCard/index.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import "./style.css"
+
+const BookCard = ({ img, name, author, key }) => {
+    return (
+        <article className="book-card">
+            <figure key={key}>
+                <img src={img} alt={name} className="reactangle" /> <br />
+                <figcaption >
+                    <p className="book-name">{name}</p>
+                    <p className="author-name">{author}</p>
+                </figcaption>
+            </figure>
+        </article >
+    )
+}
+
+export default BookCard
