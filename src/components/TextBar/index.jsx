3c7a2e0af4c4d8a65086276020a864e89948ec7b
Dhanesh Vishwakarma <dhanesh.vishwakarma11@gmail.com>
2021-09-06 11:03:22 +0530

    created Text box component

---
+++ src/components/TextBar/index.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "./style.css"
+
+const TextBar = () => {
+    return (
+        <div>
+            <input type="text" name="textBar" placeholder="Search" className="text-bar" />
+        </div>
+    )
+}
+
+export default TextBar
