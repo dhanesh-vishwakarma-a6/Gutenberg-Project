32519c7a9db917fa550f0f7de9df1b2f183c4faa
Dhanesh Vishwakarma <dhanesh.vishwakarma11@gmail.com>
2021-09-07 04:14:23 +0530

    arranged code

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 import Home from "pages/Home";
 import Fiction from "pages/Fiction";
+
 function App() {
   return (
     <Router>
