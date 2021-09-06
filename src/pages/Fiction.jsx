0f262a4aaccbf72dde6e2a834623e63552f28afa
Dhanesh Vishwakarma <dhanesh.vishwakarma11@gmail.com>
2021-09-07 04:22:37 +0530

    moved all the code to the base component for proper setup

--- src/pages/Fiction.jsx
+++ src/pages/Fiction.jsx
@@ -1,17 +1,9 @@
-import React, { useState } from "react"
+import React from "react"
 import Base from "components/Base/index"
-import BookCard from "components/BookCard"
-
-import { books as booksData } from "../data"
 
 const Fiction = () => {
-    const [books, setBooks] = useState(booksData)
     return (
-        <Base genreName="fiction">
-            {books.map((book, index) => (
-                <BookCard img={book.img} name={book.name} author={book.author} key={index} />
-            ))}
-        </Base>
+        <Base genreName="fiction" />
     )
 }
 
