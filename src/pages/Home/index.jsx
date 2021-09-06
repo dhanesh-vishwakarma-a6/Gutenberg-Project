e726f916dd734dcf3f4a88cecd0da5cc3c148194
Dhanesh Vishwakarma <dhanesh.vishwakarma11@gmail.com>
2021-09-06 16:40:29 +0530

    added grid layout to the page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,19 +4,21 @@ import "./style.css"
 
 const Home = ({ genres }) => {
     return (
-        <main>
+        <div className="home-page">
             <header className="title-container">
                 <section className="title">
                     <h1>gutenberg project</h1>
                     <p>A social cataloging website that allows you to freely search its database of books, annotations, and reviews.</p>
                 </section>
             </header>
-            <section className="genre-container">
-                {genres.map(genre => (
-                    <GenreCard name={genre.name} icon={genre.icon} />
-                ))}
-            </section>
-        </main>
+            <main className="genres-container">
+                <section className="genres">
+                    {genres.map(genre => (
+                        <GenreCard name={genre.name} icon={genre.icon} />
+                    ))}
+                </section>
+            </main>
+        </div>
     )
 }
 
