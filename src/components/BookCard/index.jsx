c9bb6abe1562b57817f18d53bdd384ae77a537a4
Dhanesh Vishwakarma <dhanesh.vishwakarma11@gmail.com>
2021-09-07 04:20:59 +0530

    kept alt prop empty to display a empty card when no image available

--- src/components/BookCard/index.jsx
+++ src/components/BookCard/index.jsx
@@ -5,7 +5,7 @@ const BookCard = ({ img, name, author, key }) => {
     return (
         <article className="book-card">
             <figure key={key}>
-                <img src={img} alt={name} className="reactangle" /> <br />
+                <img src={img} alt="" className="reactangle" /> <br />
                 <figcaption >
                     <p className="book-name">{name}</p>
                     <p className="author-name">{author}</p>
