dc00b4989e4ed926d9091a42bbf9a5e2c750961f
Dhanesh Vishwakarma <dhanesh.vishwakarma11@gmail.com>
2021-09-06 14:25:56 +0530

    added props & removed genre data

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,16 +2,7 @@ import React from "react"
 import GenreCard from "components/GenreCard"
 import "./style.css"
 
-const Home = () => {
-    const genres = [
-        { name: "fiction", icon: "" },
-        { name: "philosophy", icon: "" },
-        { name: "drama", icon: "FaTheaterMasks" },
-        { name: "history", icon: "" },
-        { name: "humour", icon: "FaLaugh" },
-        { name: "adventure", icon: "" },
-        { name: "politics", icon: "" },
-    ]
+const Home = ({ genres }) => {
     return (
         <main>
             <header className="title-container">
