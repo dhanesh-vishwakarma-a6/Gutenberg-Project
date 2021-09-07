f098badd964a14e153da12c33cc2be9b9788a431
Dhanesh Vishwakarma <dhanesh.vishwakarma11@gmail.com>
2021-09-07 10:51:05 +0530

    added key in the article tag

--- src/components/BookCard/index.jsx
+++ src/components/BookCard/index.jsx
@@ -3,9 +3,9 @@ import "./style.css"
 
 const BookCard = ({ id, img, name, author, }) => {
     return (
-        <article className="book-card">
-            <figure key={id}>
-                <img src={img} alt="" className="reactangle" /> <br />
+        <article key={id} className="book-card">
+            <figure >
+                <img src={img} alt={name} className="reactangle" /> <br />
                 <figcaption >
                     <p className="book-name">{name}</p>
                     <p className="author-name">{author}</p>
