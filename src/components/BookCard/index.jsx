eb6cc9140436553050b2258217f22b0322ed72d7
Dhanesh Vishwakarma <dhanesh.vishwakarma11@gmail.com>
2021-09-07 17:53:19 +0530

    made the book cover as an anchor to visit the book content page

--- src/components/BookCard/index.jsx
+++ src/components/BookCard/index.jsx
@@ -1,11 +1,14 @@
 import React from "react"
 import "./style.css"
 
-const BookCard = ({ id, img, name, author, }) => {
+
+const BookCard = ({ id, img, name, author, viewBook }) => {
     return (
         <article key={id} className="book-card">
             <figure >
-                <img src={img} alt={name} className="reactangle" /> <br />
+                <a href={viewBook} target="_blank" rel="noopener noreferrer">
+                    <img src={img} alt={name} className="reactangle" /> <br />
+                </a>
                 <figcaption >
                     <p className="book-name">{name}</p>
                     <p className="author-name">{author}</p>
