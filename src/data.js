caca90e3ac22f3fc10a555f249e1c984da6cba3f
Dhanesh Vishwakarma <dhanesh.vishwakarma11@gmail.com>
2021-09-07 13:05:15 +0530

    added genres

---
+++ src/data.js
@@ -0,0 +1,9 @@
+export const genres = [
+    { name: "fiction", icon: "" },
+    { name: "philosophy", icon: "" },
+    { name: "drama", icon: "FaTheaterMasks" },
+    { name: "history", icon: "" },
+    { name: "humour", icon: "FaLaugh" },
+    { name: "adventure", icon: "" },
+    { name: "politics", icon: "" },
+]
