da53463e54a40ebe75c3f8f8dc3c22b268f62001
Dhanesh Vishwakarma <dhanesh.vishwakarma11@gmail.com>
2021-09-07 21:34:39 +0530

    added all pages with lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
-import React from "react"
+import React, { Suspense, lazy } from "react"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+// component
+import Loader from "components/Loader";
+// pages
+const Home = lazy(() => import("pages/Home"))
+const Fiction = lazy(() => import("pages/Fiction"))
+const Philosophy = lazy(() => import("pages/Philosophy"))
+const Drama = lazy(() => import("pages/Drama"))
+const History = lazy(() => import("pages/History"))
+const Humour = lazy(() => import("pages/Humour"))
+const Adventure = lazy(() => import("pages/Adventure"))
+const Politics = lazy(() => import("pages/Politics"))
 
-import Home from "pages/Home";
-import Fiction from "pages/Fiction";
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/fiction" exact component={Fiction} />
-      </Switch>
+      <Suspense fallback={<Loader />}>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/fiction" exact component={Fiction} />
+          <Route path="/philosophy" exact component={Philosophy} />
+          <Route path="/drama" exact component={Drama} />
+          <Route path="/history" exact component={History} />
+          <Route path="/humour" exact component={Humour} />
+          <Route path="/adventure" exact component={Adventure} />
+          <Route path="/politics" exact component={Politics} />
+        </Switch>
+      </Suspense>
     </Router>
   );
 }
