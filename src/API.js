0ac8d71dbe7dc7682e18de557356580f507260f4
Dhanesh Vishwakarma <dhanesh.vishwakarma11@gmail.com>
2024-03-09 18:03:31 +0530

    added new api function to get all books. Made category clickable instead of the icon in it.

--- src/API.js
+++ src/API.js
@@ -1,29 +1,35 @@
 const API = process.env.REACT_APP_API_BASE
 
+export const getAllBooks = async () => {
+    return await fetch(`${API}/books`, { method: "GET" })
+        .then(response => response.json())
+        .catch(err => console.log(err))
+}
+
 // book by topic
-export const getBooksByGenre = (genreName) => {
-    return fetch(`${API}/books?topic=${genreName}`, { method: "GET" })
+export const getBooksByGenre = async (genreName) => {
+    return await fetch(`${API}/books?topic=${genreName}`, { method: "GET" })
         .then(response => response.json())
         .catch(err => console.log(err))
 }
 
 // indivisual book
-export const getBookById = (bookId) => {
-    return fetch(`${API}/books/${bookId}`, { method: "GET" })
+export const getBookById = async (bookId) => {
+    return await fetch(`${API}/books/${bookId}`, { method: "GET" })
         .then(response => response.json())
         .catch(err => console.log(err))
 }
 
 // search by string
-export const search = (str) => {
-    return fetch(`${API}/books?search=${encodeURI(str)}`, { method: "GET" })
+export const search = async (str) => {
+    return await fetch(`${API}/books?search=${encodeURI(str)}`, { method: "GET" })
         .then(response => response.json())
         .catch(err => console.log(err))
 }
 
 // search by MIME
-export const mime = (mimeType) => {
-    return fetch(`${API}/books?mime_type=text%2F${mimeType}`, { method: "GET" })
+export const mime = async (mimeType) => {
+    return await fetch(`${API}/books?mime_type=text%2F${mimeType}`, { method: "GET" })
         .then(response => response.json())
         .catch(err => console.log(err))
 }
